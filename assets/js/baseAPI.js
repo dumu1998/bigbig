cc2039fd0f9bcbe13d102f091f9efbae55f4e2d6
dumu <dumu_98@163.com>
2022-09-29 15:45:36 +0800

    完成登录注册功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,19 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
+$.ajaxPrefilter(function (options) {
+  // 将获取的表单数据转换为josn字符串
+  const form2Josn = (soure) => {
+    let target = {}
+    soure.split('&').forEach(el => {
+      let kv = el.split('=')
+      target[kv[0]] = kv[1]
+    })
+    return JSON.stringify(target)
+  }
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+  options.url = 'http://big-event-vue-api-t.itheima.net' + options.url
+  options.contentType = 'application/json'
+
+  options.data = form2Josn(options.data)
 })
--- assets/js/login1.js
+++ assets/js/login1.js
@@ -1,17 +1,51 @@
-$(function(){
+$(function () {
     // 注册登录事件
-    $('#go2Reg').on('click',function(){
+    $('#go2Reg').on('click', function () {
         $('.login-warp').hide()
         $('.reg-warp').show()
     })
-    $('#go2Login').on('click',function(){
+    $('#go2Login').on('click', function () {
         $('.reg-warp').hide()
         $('.login-warp').show()
     })
 
     let form = layui.form
-    
     form.verify({
-        pwd:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'] 
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            let pwd = $('#mima').val()
+            if (pwd !== value) return '两次密码不一致！'
+        }
+    })
+
+    // 注册表单
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: `/api/reg`,
+            data: $(this).serialize(),
+            success(res) {
+                if (res.code !== 0) return layer.msg(res.message)
+                layer.msg(res.message)
+                $('#go2Login').click()
+            }
+        })
+    })
+
+    // 登录表单
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: `/api/login`,
+            data: $(this).serialize(),
+            success(res) {
+                if (res.code !== 0) return layer.msg(res.message)
+                layer.msg(res.message)
+                localStorage.setItem('token', res.token)
+                location.href = '/home.html'
+            }
+        })
     })
 })
